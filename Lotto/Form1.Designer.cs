27498bbb7511fb190123ddf7ea19d203078988fc
Lukas <slate1233@gmail.com>
2022-04-15 08:58:33 +0200

    Initial commit

---
+++ Lotto/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Lotto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Txt_Box_1 = new System.Windows.Forms.TextBox();
+            this.Btn_Tipp_1 = new System.Windows.Forms.Button();
+            this.Btn_Ende = new System.Windows.Forms.Button();
+            this.Txt_Box_2 = new System.Windows.Forms.TextBox();
+            this.Lbl_1 = new System.Windows.Forms.Label();
+            this.Lbl_2 = new System.Windows.Forms.Label();
+            this.Txt_Box_3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Txt_Box_1
+            // 
+            this.Txt_Box_1.Location = new System.Drawing.Point(33, 74);
+            this.Txt_Box_1.Multiline = true;
+            this.Txt_Box_1.Name = "Txt_Box_1";
+            this.Txt_Box_1.Size = new System.Drawing.Size(166, 29);
+            this.Txt_Box_1.TabIndex = 0;
+            // 
+            // Btn_Tipp_1
+            // 
+            this.Btn_Tipp_1.Location = new System.Drawing.Point(444, 72);
+            this.Btn_Tipp_1.Name = "Btn_Tipp_1";
+            this.Btn_Tipp_1.Size = new System.Drawing.Size(113, 50);
+            this.Btn_Tipp_1.TabIndex = 1;
+            this.Btn_Tipp_1.Text = "Tipp";
+            this.Btn_Tipp_1.UseVisualStyleBackColor = true;
+            this.Btn_Tipp_1.Click += new System.EventHandler(this.Btn_Tipp_1_Click);
+            // 
+            // Btn_Ende
+            // 
+            this.Btn_Ende.Location = new System.Drawing.Point(363, 429);
+            this.Btn_Ende.Name = "Btn_Ende";
+            this.Btn_Ende.Size = new System.Drawing.Size(107, 46);
+            this.Btn_Ende.TabIndex = 1;
+            this.Btn_Ende.Text = "Ende";
+            this.Btn_Ende.UseVisualStyleBackColor = true;
+            this.Btn_Ende.Click += new System.EventHandler(this.Btn_Ende_Click);
+            // 
+            // Txt_Box_2
+            // 
+            this.Txt_Box_2.Location = new System.Drawing.Point(33, 120);
+            this.Txt_Box_2.Multiline = true;
+            this.Txt_Box_2.Name = "Txt_Box_2";
+            this.Txt_Box_2.Size = new System.Drawing.Size(166, 31);
+            this.Txt_Box_2.TabIndex = 0;
+            // 
+            // Lbl_1
+            // 
+            this.Lbl_1.AutoSize = true;
+            this.Lbl_1.Location = new System.Drawing.Point(287, 77);
+            this.Lbl_1.Name = "Lbl_1";
+            this.Lbl_1.Size = new System.Drawing.Size(0, 13);
+            this.Lbl_1.TabIndex = 2;
+            // 
+            // Lbl_2
+            // 
+            this.Lbl_2.AutoSize = true;
+            this.Lbl_2.Location = new System.Drawing.Point(287, 112);
+            this.Lbl_2.Name = "Lbl_2";
+            this.Lbl_2.Size = new System.Drawing.Size(0, 13);
+            this.Lbl_2.TabIndex = 2;
+            // 
+            // Txt_Box_3
+            // 
+            this.Txt_Box_3.Location = new System.Drawing.Point(33, 172);
+            this.Txt_Box_3.Multiline = true;
+            this.Txt_Box_3.Name = "Txt_Box_3";
+            this.Txt_Box_3.Size = new System.Drawing.Size(166, 32);
+            this.Txt_Box_3.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 550);
+            this.Controls.Add(this.Lbl_2);
+            this.Controls.Add(this.Lbl_1);
+            this.Controls.Add(this.Btn_Ende);
+            this.Controls.Add(this.Btn_Tipp_1);
+            this.Controls.Add(this.Txt_Box_2);
+            this.Controls.Add(this.Txt_Box_3);
+            this.Controls.Add(this.Txt_Box_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Txt_Box_1;
+        private System.Windows.Forms.Button Btn_Tipp_1;
+        private System.Windows.Forms.Button Btn_Ende;
+        private System.Windows.Forms.TextBox Txt_Box_2;
+        private System.Windows.Forms.Label Lbl_1;
+        private System.Windows.Forms.Label Lbl_2;
+        private System.Windows.Forms.TextBox Txt_Box_3;
+    }
+}
+
---
+++ Lotto/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lotto
+{
+    public partial class Form1 : Form
+    {
+
+        
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Tipp_1_Click(object sender, EventArgs e)
+        {
+            List<int> userNums = new List<int>();
+            List<int> lottoNums = new List<int>();
+
+            userNums.Add(Convert.ToInt32(Txt_Box_1.Text));
+            userNums.Add(Convert.ToInt32(Txt_Box_2.Text));
+            userNums.Add(Convert.ToInt32(Txt_Box_3.Text));
+<
+            Random lottoNums = new Random();
+
+            lottoNums.Add(lottoNums.Next(1, 4));
+            lottoNums.Add(lottoNums.Next(1, 5));
+            lottoNums.Add(lottoNums.Next(1, 6));
+
+            lottoNums.Remove(userNums[0]);
+            lottoNums.Remove(userNums[1]);
+            lottoNums.Remove(userNums[2]);
+
+            if (lottoNums.Count == 0)
+                Lbl_1.Text = "You didnt get any matches";
+            else if (lottoNums.Count == 1)
+                Lbl_1.Text = "You made one match!";
+            else if (lottoNums.Count == 2)
+                Lbl_1.Text = "You made two matches!";
+            else if (lottoNums.Count == 3)
+                Lbl_1.Text = "You made three matches, jackpot!";
+
+            
+        }
+           
+
+       
+       
+
+        private void Btn_Ende_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
